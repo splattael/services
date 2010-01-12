02df6350e671a18a908c962ada611c893358dede
Peter Suschlik <peter@suschlik.de>
2010-01-12 18:13:55 +0100

    Nacktscanner-Mitzeichner.

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'sinatra'
 require 'RMagick'
 require 'httparty'
+require 'nokogiri'
 
 CACHE={}
 
@@ -25,6 +26,29 @@ get '/petition.gif' do
     # [ { :text => "96% / 48372 Mitzeichner (10 pro Minute)", ... } ]
     json = HTTParty.get("http://twitter.com/statuses/user_timeline/37667542.json?count=3") || []
     text = (json.detect {|hash| /(\d+)%/ =~ hash["text"] } || {})["text"]
+
+    image = Magick::Image.new(230, 16)
+    image.format = "GIF"
+    draw = Magick::Draw.new
+    draw.text(5, 12, text)
+    draw.draw(image)
+
+    image.to_blob
+  end
+end
+
+get '/nacktscanner.gif' do
+  expires = 20
+  content_type "image/gif"
+  response['Expires'] = (Time.now + expires * 10).httpdate
+
+  cached(:nacktscanner, expires) do
+    html = HTTParty.get("https://epetitionen.bundestag.de/index.php?action=petition;sa=details;petition=9109")
+    doc = Nokogiri(html).at(".pet_det_td_4")
+    mitzeichner = doc.children[0].to_s.to_i
+    max = 50_000
+    per_cent = mitzeichner * 100.0 / max
+    text = "%d%% / %d gegen Nacktscanner" % [ per_cent, mitzeichner ]
 
     image = Magick::Image.new(230, 16)
     image.format = "GIF"
